c465c18dba5dd9e7dc02d1c820623ca9044aec63
unknown <hafizmursiddd@gmail.com>
2023-01-30 21:30:09 +0700

    menambahkan folder advanced dan project

---
+++ AdvancedCSharp/Generic/MyList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp.Generic
+{
+    internal class MyList
+    {
+        public static void DisplayListString() { 
+        
+        List<string> list = new List<string>();
+
+            list.Add("Code Id");
+            list.Add("Bootcamp");
+            list.Add("DotNet");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        public static void DisplayListInt()
+        {
+
+            List<int> list = new List<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+
+    }
+}
---
+++ AdvancedCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdvancedCSharp.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        MyList.DisplayListString();
+        MyList.DisplayListInt();
+
+
+
+        Person <int> person = new Person<int>();
+        var a = person.DisplayInformation(12345);
+        Console.WriteLine("Person Int : "+a);
+
+        Person <String> person2 = new ();
+        var b = person2.DisplayInformation("Hello, From List Generic");
+        Console.WriteLine("Person string : " + b);
+    }
+}
